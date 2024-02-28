28f99d539b63a13f30aa94b9f8f987dbe8af0cf0
Akash Arun Tayade <akashtayade1504@gmail.com>
2024-02-28 21:51:09 +0530

    "Modals Defination"

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,//cloudinary url
+    },
+    programOpt:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"program"
+    }],
+    programcompleted:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"program"
+    }],
+    transactionOpt:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"transaction"
+    }]
+}, 
+
+{
+    timestamps: true
+});
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
