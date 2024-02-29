24c626dc6583c4850a6be4289c7218c69b81f119
Abbhishekk <abhishek.bante.cse@ghrce.raisoni.net>
2024-02-29 13:35:02 +0530

    Added navbar and hero section(incomplete), defined routes

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,5 @@
+import logoBlack from "./logo-black.svg";
+import logoWhite from "./logo-white.svg";
+import logo from "./logo-no-background.svg";
+import logoColor from "./logo-color.svg";
+export { logo, logoBlack, logoWhite, logoColor };
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,9 @@
+
+
+const Hero = () => {
+  return (
+    <div className="text-[#eee49d] bg-[#b99b68] w-full min-h-screen flex justify-center items-center" >Hero</div>
+  )
+}
+
+export default Hero
