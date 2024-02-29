8992049ba52e396976f5e75f1d09b3681fdcfcfa
Akash Arun Tayade <akashtayade1504@gmail.com>
2024-02-29 12:39:41 +0530

    "user model 2"

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema({
     firstname: {
         type: String,
@@ -40,6 +41,40 @@ const userSchema = new mongoose.Schema({
 {
     timestamps: true
 });
+
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")) return next;
+
+    this.password= await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.method.generateAccessToken= function (){
+   return  jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            firstname:this.firstname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:ACCESS_TOKEN_EXPIRY
+        }
+
+   )
+}
+
+userSchema.method.generateAccessToken= function (){
+    return  jwt.sign(
+         {
+             _id:this._id,
+         },
+         process.env.REFRESH_TOKEN_SECRET,
+         {
+             expiresIn:REFRESH_TOKEN_EXPIRY
+         }
+    )
+ }
 
 const user = mongoose.model("user", userSchema);
 
