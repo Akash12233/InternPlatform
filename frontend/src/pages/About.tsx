76d2f2778082874d16a7530ffd64f4c2be0f548b
Abbhishekk <abhishek.bante.cse@ghrce.raisoni.net>
2024-03-06 09:44:34 +0530

    changed background color of about section

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,11 +3,11 @@ import { abhishek, akash } from "../assets"
 
 const About: React.FC = () => {
   return (
-    <section className=" md:p-10 flex md:flex-row flex-col items-center gap-10 justify-between" >
+    <section className=" md:p-10 p-5 pb-10 flex md:flex-row flex-col items-center gap-10 justify-between bg-black text-white"  >
         <div className="md:w-1/2 w-full  mx-auto text-black" >
-          <h1 className="text-3xl text-[#eee49d]" >Our people make us great</h1>
-          <p>Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value.</p>
-          <p>Working here means you'll interact with talented professionals, will be challenged to solve difficult problems and think in new and creative ways. Along the way, you may also make some new friends, have a lot of fun and enjoy opportunities.</p>
+          <h1 className="text-3xl text-[#eee49d] font-bold" >Our people make us great</h1>
+          <p className="text-gray-50">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value.</p>
+          <p className="text-gray-50">Working here means you'll interact with talented professionals, will be challenged to solve difficult problems and think in new and creative ways. Along the way, you may also make some new friends, have a lot of fun and enjoy opportunities.</p>
         </div>
         <div className="md:w-1/2 w-full mt-10 md:mt-2 mx-auto" >
           <div className="flex gap-5" >
@@ -17,7 +17,7 @@ const About: React.FC = () => {
               <p className="text-sm text-slate-50  " >Full Stack Developer</p>
               <p className="text-sm text-slate-50  mt-5" >
                   A full-stack developer like Abhishek builds both the user-facing and server-side aspects of web applications.</p>
-              <p className="flex gap-2 mt-5" >
+              <p className="flex gap-2 mt-5 bg-[#eee49d] rounded-full p-2 w-36" >
                 <a href="https://www.linkedin.com/in/abhishek-bante/" target="_blank" className="text-sm text-slate-50" ><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
                       <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
                   </svg>
@@ -36,7 +36,7 @@ const About: React.FC = () => {
                 </a>
 
                 <a href="https://abbhishek-portfolio.netlify.app/" target="_blank">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                     </svg>
 
@@ -53,8 +53,8 @@ const About: React.FC = () => {
               <p className="text-sm text-slate-50  " >Product Manager</p>
               <p className="text-sm text-slate-50  mt-5" >
               Akash, the product manager, is responsible for the strategy, development, and success of a specific product or product line.</p>
-              <p className="flex gap-2 mt-5" >
-                <a href="https://www.linkedin.com/in/akash-tayade-23453322a/" target="_blank" className="text-sm text-slate-50" ><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
+              <p className="flex gap-2 mt-5 bg-[#eee49d] rounded-full p-2 w-36" >
+                <a href="https://www.linkedin.com/in/akash-tayade-23453322a/" target="_blank" className="text-sm " ><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
                       <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
                   </svg>
                 </a>
@@ -90,7 +90,7 @@ const About: React.FC = () => {
               <p className="text-sm text-slate-50  " >Full Stack Developer</p>
               <p className="text-sm text-slate-50  mt-5" >
                   A full-stack developer like Abhishek builds both the user-facing and server-side aspects of web applications.</p>
-              <p className="flex gap-2 mt-5" >
+              <p className="flex gap-2 mt-5 bg-[#eee49d] rounded-full p-2 w-36" >
                 <a href="https://www.linkedin.com/in/abhishek-bante/" target="_blank" className="text-sm text-slate-50" ><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
                       <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
                   </svg>
@@ -109,7 +109,7 @@ const About: React.FC = () => {
                 </a>
 
                 <a href="https://abbhishek-portfolio.netlify.app/" target="_blank">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                     </svg>
 
