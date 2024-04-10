4bcea2963d8e9d28420a3ce481f5e936489f027b
Abbhishekk <abhishek.bante.cse@ghrce.raisoni.net>
2024-04-10 11:15:42 +0530

    added cors options

--- backend/src/controllers/program.controller.js
+++ backend/src/controllers/program.controller.js
@@ -1,4 +1,5 @@
 import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import program from "../models/program.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
